49b738b33fd957eeb89f533d7a217daba33fa2b3
matiascazon <matiashcazon@gmail.com>
2023-11-06 16:43:39 -0300

    details

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -53,23 +53,23 @@ const Detail = () => {
           <h1 className="text-2xl text-center font-bold mt-5 mb-3 uppercase">Detalles</h1>
           <div className="m-2 rounded-2xl sm:flex sm:justify-center">
             <img src={image} alt="Foto de perfil del profesional" className="rounded-t-2xl sm:w-1/2 sm:rounded-l-3xl sm:rounded-tr-none md:min-h-min" />
-            <div className=" px-4 py-2 rounded-b-3xl bg-gray-50 flex flex-col gap-y-6 rounded-r-3xl sm:rounded-bl-none">
+            <div className=" px-4 py-2 rounded-b-3xl bg-gray-50 flex flex-col gap-y-6 rounded-r-3xl sm:rounded-bl-none dark:bg-neutral-700">
               <div className="flex flex-col items-center">
-                <h2 className="font-bold text-3xl text-black text-left my-1">{user.name}</h2>
+                <h2 className="font-bold text-3xl text-black text-left my-1 dark:text-white">{user.name}</h2>
                 <div className="flex gap-x-2 sm:flex-col sm:gap-y-2">
-                  <p className="font-semibold text-lg my-1">Username: <span className=" text-gray-500">{user.username}</span></p>
-                  <p className="font-semibold text-lg my-1">email: <span className=" text-gray-500">{user.email}</span></p>
+                  <p className="font-semibold text-lg my-1">Username: <span className=" text-gray-500 dark:text-gray-300">{user.username}</span></p>
+                  <p className="font-semibold text-lg my-1">email: <span className=" text-gray-500 dark:text-gray-300">{user.email}</span></p>
                 </div>
               </div>
               <div className="flex justify-between sm:flex-col sm:gap-y-6">
                 <div>
                 <h2 className="font-bold text-xl my-1">Contacto</h2>
-                  <p className="font-semibold text-lg my-1">Télefono: <span className=" text-gray-500">{user.phone}</span></p>
-                  <p className="font-semibold text-lg my-1">Website: <span className=" text-blue-600 cursor-pointer">{user.website}</span></p>
+                  <p className="font-semibold text-lg my-1">Télefono: <span className=" text-gray-500 dark:text-gray-300">{user.phone}</span></p>
+                  <p className="font-semibold text-lg my-1">Website: <span className=" text-blue-600 cursor-pointer dark:text-gray-300">{user.website}</span></p>
                 </div>
                 <div>
                   <h2 className="font-bold text-xl my-1">Empresa</h2>
-                  <p className="my-1 text-gray-500 font-semibold text-lg">{user.company.name}</p>
+                  <p className="my-1 text-gray-500 font-semibold text-lg dark:text-gray-300">{user.company.name}</p>
                   <p></p>
                 </div>
               </div>
